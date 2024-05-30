e9287b5b587a74e2fd5492e129e9238e0dbfd9ca
gilorah <fabrigilorah@hotmail.com>
2024-05-30 17:30:21 +0200

    help

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 //json array movement variable
 var i = 0;
 var correctCount = 0;
+//initialize the first question
+generate(0);
 // generate from json array data with index
 function generate(index){
 document.getElementById("question").innerHTML = jsonData[index].q ;
